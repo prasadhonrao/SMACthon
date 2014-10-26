c21c40e0d4dbf02f2df491a4e87fbc6584e6783a
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-10-26 11:07:53 -0400

    Code corrected

--- NotificadoR/NotificadoR.Angular/app/dashboard/dashboard.js
+++ NotificadoR/NotificadoR.Angular/app/dashboard/dashboard.js
@@ -1,41 +1,45 @@
-﻿'use strict';
-angular.module('app').controller(controllerId, ['common', 'datacontext', dashboard]);
+﻿(function () {
+    'use strict';
 
-function dashboard(common, datacontext) {
-    var getLogFn = common.logger.getLogFn;
-    var log = getLogFn(controllerId);
-
-    var vm = this;
-    vm.toDoItems = [];
-    vm.title = 'Dashboard';
     var controllerId = 'dashboard';
     var chat = $.connection.ngHub;
-    
-    chat.client.push = function (message) {
-        getToDoItems();
-        toastr.info(message);
-    };
 
-    activate();
+    angular.module('app').controller(controllerId, ['common', 'datacontext', dashboard]);
 
-    function activate() {
-        var promises = [getToDoItems()];
-        common.activateController(promises, controllerId)
-            .then(function () {
-                log('Activated Dashboard View');
-                $.connection.hub.start()
-                         .done(function () {
-                             chat.server.send('Updating clients');
-                         });
+    function dashboard(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
 
+        var vm = this;
+        vm.toDoItems = [];
+        vm.title = 'Dashboard';
+
+        chat.client.push = function (message) {
+            getToDoItems();
+            toastr.info(message);
+        };
+
+        activate();
+
+        function activate() {
+            var promises = [getToDoItems()];
+            common.activateController(promises, controllerId)
+                .then(function () {
+                    log('Activated Dashboard View');
+                    $.connection.hub.start()
+                             .done(function () {
+                                 chat.server.send('Updating clients');
+                             });
+
+                });
+        }
+
+        function getToDoItems() {
+            return datacontext.getToDoItems().then(function (data) {
+                //chat.server.send('Updating clients');
+                return vm.toDoItems = data;
             });
-    }
+        }
 
-    function getToDoItems() {
-        return datacontext.getToDoItems().then(function (data) {
-            //chat.server.send('Updating clients');
-            return vm.toDoItems = data;
-        });
     }
-
-}
+})();
