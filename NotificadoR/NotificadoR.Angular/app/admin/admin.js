19ddeaaf219a54a1311013f6b1dc6db5ee5e9279
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-10-26 11:34:43 -0400

    Admin view integrated

--- NotificadoR/NotificadoR.Angular/app/admin/admin.js
+++ NotificadoR/NotificadoR.Angular/app/admin/admin.js
@@ -9,12 +9,24 @@
 
         var vm = this;
         vm.title = 'Admin';
+        vm.submit = submit;
+        vm.documentTitle = "";
+        vm.documentAbstract = "";
 
         activate();
 
         function activate() {
             common.activateController([], controllerId)
                 .then(function () { log('Activated Admin View'); });
+        }
+
+        function submit() {
+            //console.log(vm.title + vm.abstract);
+            log(vm.documentTitle + vm.documentAbstract);
+            var client = new WindowsAzure.MobileServiceClient('https://notificador.azure-mobile.net/', 'bbiIcbESnkRJqRxaMlppCYpoxrHQbm31'),
+                todoItemTable = client.getTable('todoitem');
+            todoItemTable.insert({ text: vm.documentTitle, complete: false });
+            return;
         }
     }
 })();
