3e009f2b3f1cd1ff6a4c3bbb26111ec7509f8173
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-10-26 05:27:58 -0400

    HTML project added

---
+++ NotificadoR/NotificadoR.HTML/NotificadoRHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotificadoR.HTML
+{
+    [Microsoft.AspNet.SignalR.Hubs.HubName("notificadoRHTMLHub")]
+    public class NotificadoRHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.broadcastMessage(message);
+        }
+    }
+}
